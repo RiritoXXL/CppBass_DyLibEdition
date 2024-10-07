4ae7d2fa029f9321245c806848e74b2f3e6f4dff
Ririto Ninigaya <riritoxxl23@outlook.com>
2024-10-07 23:23:22 +0300

    Update CppBass.cpp

--- CppBass-DyLib/CppBass.cpp
+++ CppBass-DyLib/CppBass.cpp
@@ -5,7 +5,7 @@ int main() {
 	SetConsoleTitleA("CppBass-DylibEdition by RiritoXXL");
 	BASS_INIT();
 	BASS_Start();
-	uint32_t handle_bass = BASS_StreamCreateFile(stringexample);
+	uint32_t handle_bass = BASS_StreamCreateFile(stringexample, BASS_LOOP);
 	BASS_ChannelPlay(handle_bass);
 	while (true) {
 		Sleep(4000);
